e7d95385ef0a7b6b251a2a2156163dd00d5c6e82
Ali Ashraf Ali <ali_90082@aucegypt.edu>
2022-07-04 14:05:49 +0200

    using FS with async calls

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 
 const path = "F:/AUC/Summer 22'/1_Intro_to_Cloud_Computing";
-const files = fs.readdirSync(path);
-console.log(files);
+// const files = fs.readdirSync(path);
+// console.log(files);
+
+fs.readdir(path, function(err, files){
+    if (err) {
+        console.log("Error: " + err);
+    }
+    else{
+        console.log("Result: " + files)
+    }
+})
